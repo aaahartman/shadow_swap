c4a8ac91797b0b44f50b6d83ad14b26f1e269a3e
Ting Cheng <chengt4@uw.edu>
2017-04-29 22:51:51 -0700

    basic player/shadow swap and movement

--- ShadowSwap/source/PlayState.hx
+++ ShadowSwap/source/PlayState.hx
@@ -9,25 +9,37 @@ import flixel.addons.editors.ogmo.FlxOgmoLoader;
 
 class PlayState extends FlxState
 {
-	private var _player:Player;
-	private var _map:FlxOgmoLoader;
- 	private var _mWalls:FlxTilemap;
+	private var _player1:Player;
+	private var _player2:Player;
+	//private var _map:FlxOgmoLoader;
+ 	//private var _mWalls:FlxTilemap;
 
 
 	override public function create():Void
 	{
-		_map = new FlxOgmoLoader(AssetPaths.room_001__oel);
- 		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
- 		_mWalls.follow();
- 		_mWalls.setTileProperties(1, FlxObject.NONE);
- 		_mWalls.setTileProperties(2, FlxObject.ANY);
- 		add(_mWalls);
-		_player = new Player();
- 		_map.loadEntities(placeEntities, "entities");
- 		add(_player);
+		//_map = new FlxOgmoLoader(AssetPaths.room_001__oel);
+ 		//_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
+ 		//_mWalls.follow();
+ 		//_mWalls.setTileProperties(1, FlxObject.NONE);
+ 		//_mWalls.setTileProperties(2, FlxObject.ANY);
+ 		//add(_mWalls);
+
+
+ 		/* TODO: Change the x, y initial parameters back to 0. and 
+ 		set starting location using Tile map. */
+		_player1 = new Player(60, 100, false);  //player
+		_player2 = new Player(50, 100, true);  //shadow
+
+
+ 		//_map.loadEntities(placeEntities, "entities");
+
+
+ 		add(_player1);
+ 		add(_player2);
+
 		super.create();
 	}
-
+/*
 	private function placeEntities(entityName:String, entityData:Xml):Void
 	{
 	    var x:Int = Std.parseInt(entityData.get("x"));
@@ -38,10 +50,10 @@ class PlayState extends FlxState
 	        _player.y = y;
 	    }
 	}
-
+*/
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		FlxG.collide(_player, _mWalls);
+		//FlxG.collide(_player, _mWalls);
 	}
 }
--- ShadowSwap/source/Player.hx
+++ ShadowSwap/source/Player.hx
@@ -8,50 +8,105 @@ import flixel.math.FlxPoint;
 
 class Player extends FlxSprite
 {
+	private var _is_shadow:Bool = false;
 	public var speed:Float = 200;
+	public var gravity:Float = 200;
+	public var jump_speed:Float = 200;
+	private var jump_duration:Float = -1;
 
-    public function new(?X:Float=0, ?Y:Float=0)
+    public function new(?X:Float=0, ?Y:Float=0, ?is_shadow:Bool)
     {
+    	_is_shadow = is_shadow;
         super(X, Y);
-        makeGraphic(8, 14, FlxColor.BLUE);
+
+        if (_is_shadow)
+        	makeGraphic(8, 14, FlxColor.BLUE);
+        else 
+        	makeGraphic(8, 14, FlxColor.RED);
+        
         drag.x = drag.y = 1600;
     }
 
-    private function movement():Void
-    {
-    	var _up:Bool = false;
+	override public function update(elapsed:Float):Void
+	{
+		// Swap between player and shadow if "S" is pressed
+		var _swap:Bool = false;
+		_swap = FlxG.keys.justReleased.S;
+		if (_swap) {
+			_is_shadow = !_is_shadow;
+
+			// update player/shadow graphics
+			if (_is_shadow)
+        		makeGraphic(8, 14, FlxColor.BLUE);
+        	else 
+        		makeGraphic(8, 14, FlxColor.RED);
+        }
+
+        acceleration.x = 0;
+        acceleration.y = gravity;
+
+        // move player only
+		if (!_is_shadow)
+	    	movement(elapsed);
+
+	    super.update(elapsed);
+	}
+
+	private function movement(elapsed:Float):Void {
+    	var _jump:Bool = false;
  		var _left:Bool = false;
  		var _right:Bool = false;
 
- 		_up = FlxG.keys.anyPressed([UP, W]);
+ 		_jump = FlxG.keys.anyPressed([UP, W]);
  		_left = FlxG.keys.anyPressed([LEFT, A]);
  		_right = FlxG.keys.anyPressed([RIGHT, D]);
+
+ 		acceleration.x = 0;
 
  		if (_left && _right) {
       		_left = _right = false;
  		}
 
- 		if (_up || _left || _right) {
- 		 	var mA:Float = 0;
-			if (_up)
-			{
-			    mA = -90;
-			    if (_left)
-			        mA -= 45;
-			    else if (_right)
-			        mA += 45;
-			}
-			else if (_left){
-			    mA = 180;
-			}
-			velocity.set(speed, 0);
- 			velocity.rotate(FlxPoint.weak(0, 0), mA);
+
+		//var mA:Float = 0;
+		if (_jump) {
+			jump(elapsed);
 		}
+		    //mA = -90;
+		    //if (_left)
+		    //    mA -= 45;
+		    //else if (_right)
+		    //    mA += 45;
+		//}
+		if (_left) {
+		    velocity.x = -speed;
+		    acceleration.x = -drag.x;
+		} else if (_right) {
+			velocity.x = speed;
+			acceleration.x = drag.x;
+		}
+		//velocity.set(speed, 0);
+		//velocity.rotate(FlxPoint.weak(0, 0), mA);
 	}
 
-	override public function update(elapsed:Float):Void
-	{
-	    movement();
-	    super.update(elapsed);
+	private function jump(elapsed:Float):Void {
+		// Allow jump if player is on the ground
+		//if ( velocity.y == 0) {
+			jump_duration = 0;
+		//}
+
+		if (jump_duration >= 0) {
+			jump_duration += elapsed;
+
+			// Max time in air in 0.25 seconds
+			if (jump_duration > 0.25)
+				jump_duration = -1;
+			else 
+				velocity.y = -0.98 * jump_speed;
+
+		} else {
+			jump_duration = -1.0;
+		}
 	}
+	
 }

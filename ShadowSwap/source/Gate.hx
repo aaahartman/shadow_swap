5d9902d3ea9f44fb298a2137275b1310f395186e
Yina Zhu <yinazhu@cs.washington.edu>
2017-05-04 23:42:34 -0700

    set gate entitle to have fixed height, adjust level 4

--- ShadowSwap/source/Gate.hx
+++ ShadowSwap/source/Gate.hx
@@ -8,7 +8,7 @@ class Gate extends FlxSprite
 	public function new(?X:Float = 0, ?Y:Float = 0)
 	{
 		super(X, Y);
-		makeGraphic(16, 16, FlxColor.GREEN);
+		makeGraphic(16, 64, FlxColor.GREEN);
 		immovable = true;
 	}
 }

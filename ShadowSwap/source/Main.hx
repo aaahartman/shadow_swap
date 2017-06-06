351e2b05ffa79c83a25414bd4d6cf2e4f4de9b9d
Yina Zhu <yinazhu@cs.washington.edu>
2017-06-05 18:11:03 -0700

    syntax fix

--- ShadowSwap/source/Main.hx
+++ ShadowSwap/source/Main.hx
@@ -13,8 +13,8 @@ class Main extends Sprite
 		Reg.initSave();
 
 		// Start the game at the highest level solved
- +		var level = Reg.getNumUnlockedLevels();
- +		Reg.setCurrentLevel(level);
+ 		var level = Reg.getNumUnlockedLevels();
+ 		Reg.setCurrentLevel(level);
 		addChild(new FlxGame(736, 640, PlayState, 1, 60, 60, true, false));
 	}
 }
